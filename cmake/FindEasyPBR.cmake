bfcc015ffbf335c2f8e3afafeda2efc345981ae9
Radu Alexandru Rosu <alejandro.rosu@gmail.com>
2020-01-23 13:27:08 +0000

    Uses the pybind from EasyPBR therefore ensuring that it is the same version

--- cmake/FindEasyPBR.cmake
+++ cmake/FindEasyPBR.cmake
@@ -11,14 +11,13 @@ execute_process(
   OUTPUT_VARIABLE PYTHON_SITE
   OUTPUT_STRIP_TRAILING_WHITESPACE)
 # message("--------------------PYTHON is at ", ${PYTHON_SITE})
-#Get only the first line of the egg_link because that is pointing towards the easypbrsrc
+#Get only the first line of the egg_link because that is pointing towards the easypbr src
 execute_process (
-    # COMMAND bash -c "date +'%F %T'" "${PYTHON_SITE}/easypbr.egg-link"
     COMMAND bash -c "head -1 $0" "${PYTHON_SITE}/easypbr.egg-link"
     OUTPUT_VARIABLE EASYPBR_SRC_PATH
     OUTPUT_STRIP_TRAILING_WHITESPACE
 )
-message("EASYPPBR source is ", ${EASYPBR_SRC_PATH})
+# message("EASYPPBR source is ", ${EASYPBR_SRC_PATH})
 
 #DEBUG
 # set(EASYPBR_SRC_PATH "/media/rosu/Data/phd/c_ws/src/easy_pbr")
@@ -32,8 +31,9 @@ find_path(EASYPBR_CONCURRENTQUEUE_INCLUDE_DIR  NAMES concurrentqueue.h PATHS "${
 find_path(EASYPBR_IMGUI_INCLUDE_DIR  NAMES imgui.h PATHS "${EASYPBR_SRC_PATH}/deps/imgui" )
 find_path(EASYPBR_IMGUIZMO_INCLUDE_DIR  NAMES ImGuizmo.h PATHS "${EASYPBR_SRC_PATH}/deps/imguizmo" )
 find_path(EASYPBR_BETTERENUM_INCLUDE_DIR  NAMES enum.h PATHS "${EASYPBR_SRC_PATH}/deps/better_enums" )
+find_path(EASYPBR_PYBIND_INCLUDE_DIR  NAMES pybind11/pybind11.h PATHS "${EASYPBR_SRC_PATH}/deps/pybind11/include" ) #important because any module that tries to have python modules that interact with easypbr should be built with the same pybind version
 
-set( EASYPBR_INCLUDE_DIR ${EASYPBR_SRC_PATH}/extern ${EASYPBR_CORE_INCLUDE_DIR} ${EASYPBR_EASYGL_INCLUDE_DIR} ${EASYPBR_LOGURU_INCLUDE_DIR} ${EASYPBR_CONFIGURU_INCLUDE_DIR} ${EASYPBR_UTILS_INCLUDE_DIR} ${EASYPBR_CONCURRENTQUEUE_INCLUDE_DIR} ${EASYPBR_IMGUI_INCLUDE_DIR} ${EASYPBR_IMGUIZMO_INCLUDE_DIR} ${EASYPBR_BETTERENUM_INCLUDE_DIR} )
+set( EASYPBR_INCLUDE_DIR ${EASYPBR_SRC_PATH}/extern ${EASYPBR_CORE_INCLUDE_DIR} ${EASYPBR_EASYGL_INCLUDE_DIR} ${EASYPBR_LOGURU_INCLUDE_DIR} ${EASYPBR_CONFIGURU_INCLUDE_DIR} ${EASYPBR_UTILS_INCLUDE_DIR} ${EASYPBR_CONCURRENTQUEUE_INCLUDE_DIR} ${EASYPBR_IMGUI_INCLUDE_DIR} ${EASYPBR_IMGUIZMO_INCLUDE_DIR} ${EASYPBR_BETTERENUM_INCLUDE_DIR} ${EASYPBR_PYBIND_INCLUDE_DIR})
 
 # message("--------------------EASYPPBR include dir is at ", ${EASYPBR_INCLUDE_DIR})
 
@@ -44,5 +44,8 @@ find_library(EASYPBR_LIBRARY
     DOC "The EasyPBR lib directory"
     NO_DEFAULT_PATH)
 # message("--------------------EASYPPBR lib dir is at ", ${EASYPBR_LIBRARY})
+
+#needs this definitions because we use it in a header file for a default argument
+add_definitions( -DDEFAULT_CONFIG="${EASYPBR_SRC_PATH}/config/default_params.cfg" )
 
 
